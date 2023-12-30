D. J.: - Implemented Linear Layer + Complete Neural Network. - Added more TODOS to fix numerical stability in the training process.

PyNeuralNet/activation.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from typing import Optional
 
 import numpy as np
@@ -5,7 +6,48 @@ import numpy as np
 from PyNeuralNet.module import Module
 
 
-class ReLU(Module):
+class Activation(Module):
+    """
+    A module representing an activation function.
+    """
+
+    @abstractmethod
+    def forward(self, X: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        The forward pass of the activation function.
+
+        Args:
+            X (np.ndarray):
+                The input of the activation function
+
+        Returns:
+            np.ndarray:
+                The output of the activation function (Z)
+        """
+        pass
+
+    @abstractmethod
+    def backward(self, dL_dZ: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Backward propagation of the activation function.
+
+        It returns the gradients dL/dX := dL/dZ * dZ/dX of the current activation function.
+
+        Args:
+            dL_dZ (np.ndarray):
+                The gradient dL/dZ of the previous module.
+
+        Returns:
+            np.ndarray:
+                The gradient dL/dX of the activation function
+        """
+        pass
+
+    def update(self, params: dict[str, np.ndarray]):
+        pass
+
+
+class ReLU(Activation):
     """
     Rectified Linear Unit (ReLU) Activation Module.
     It should be used for introducing non-linearity in neural networks.
@@ -17,14 +59,21 @@ class ReLU(Module):
         - dZ/dX = 1 if X > 0 else 0
     """
 
+    def __init__(self):
+        self.X = None
+
     def forward(self, X: np.ndarray, **kwargs) -> np.ndarray:
-        return np.where(X <= 0, 0, X)
+        self.X = X
+        return np.where(self.X <= 0, 0, self.X)
 
-    def gradient(self, X: np.ndarray, **kwargs) -> np.ndarray:
-        return np.where(X <= 0, 0, 1)
+    def backward(self, dL_dZ: np.ndarray, **kwargs) -> np.ndarray:
+        if self.X is None:
+            raise ValueError("You need to call forward() first, before to compute the gradients!")
+        dZ_dX = np.where(self.X <= 0, 0, 1)
+        return dL_dZ * dZ_dX
 
 
-class LeakyReLU(Module):
+class LeakyReLU(Activation):
     """
     Leaky Rectified Linear Unit (LeakyReLU) Activation Module.
     It should be used for introducing non-linearity in neural networks.
@@ -41,17 +90,23 @@ class LeakyReLU(Module):
     """
 
     def __init__(self, slope: float = 0.1):
-        assert 0.0 <= slope <= 1.0, f"Illegal slope {slope}. The argument should be in between [0.0, 1.0]!"
+        if 0.0 > slope > 1.0:
+            raise ValueError(f"Illegal slope {slope}. The argument should be in between [0.0, 1.0]!")
         self.slope = slope
+        self.X = None
 
     def forward(self, X: np.ndarray, **kwargs) -> np.ndarray:
+        self.X = X
         return np.where(X <= 0, self.slope * X, X)
 
-    def gradient(self, X: np.ndarray, **kwargs) -> np.ndarray:
-        return np.where(X <= 0, self.slope, 1)
+    def backward(self, dL_dZ: np.ndarray, **kwargs) -> np.ndarray:
+        if self.X is None:
+            raise ValueError("You need to call forward() first, before to compute the gradients!")
+        dZ_dX = np.where(self.X <= 0, self.slope, 1)
+        return dL_dZ * dZ_dX
 
 
-class Sigmoid(Module):
+class Sigmoid(Activation):
     """
     Sigmoid Activation Module.
     It should be used for introducing non-linearity in neural networks.
@@ -63,14 +118,21 @@ class Sigmoid(Module):
         - dZ/dX = Sigmoid(X) * (1 - Sigmoid(X))
     """
 
+    def __init__(self):
+        self.X = None
+
     def forward(self, X: np.ndarray, **kwargs) -> np.ndarray:
+        self.X = X
         return 1 / (1 + np.exp(-X))
 
-    def gradient(self, X: np.ndarray, **kwargs) -> np.ndarray:
-        return self.forward(X) * (1 - self.forward(X))
+    def backward(self, dL_dZ: np.ndarray, **kwargs) -> np.ndarray:
+        if self.X is None:
+            raise ValueError("You need to call forward() first, before to compute the gradients!")
+        dZ_dX = self.forward(self.X) * (1 - self.forward(self.X))
+        return dL_dZ * dZ_dX
 
 
-class Tanh(Module):
+class Tanh(Activation):
     """
     Tangens Hyperbolic Module.
     It should be used for introducing non-linearity in neural networks.
@@ -82,31 +144,50 @@ class Tanh(Module):
         - dZ/dX = 1 - Tanh(X)^2
     """
 
+    def __init__(self):
+        self.X = None
+
     def forward(self, X: np.ndarray, **kwargs) -> np.ndarray:
+        self.X = X
         return (np.exp(X) - np.exp(-X)) / (np.exp(X) + np.exp(-X))
 
-    def gradient(self, X: np.ndarray, **kwargs) -> np.ndarray:
-        return 1 - self.forward(X) ** 2
+    def backward(self, dL_dZ: np.ndarray, **kwargs) -> np.ndarray:
+        if self.X is None:
+            raise ValueError("You need to call forward() first, before to compute the gradients!")
+        dZ_dX = 1 - self.forward(self.X) ** 2
+        return dL_dZ * dZ_dX
 
 
-class Softmax(Module):
+class Softmax(Activation):
+    def __init__(self):
+        self.X = None
 
-    def forward(self, X: np.ndarray, axis: Optional[int] = None) -> np.ndarray:
+    def forward(self, X: np.ndarray, axis: Optional[int] = None, **kwargs) -> np.ndarray:
+        self.X = X
         X_max = np.max(X, axis=axis, keepdims=True)
         e_X = np.exp(X - X_max)
         return e_X / np.sum(e_X, axis=axis, keepdims=True)
 
-    def gradient(self, X: np.ndarray, axis: Optional[int] = None) -> np.ndarray:
-        # TODO: Implement here
+    def backward(self, dL_dZ: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        assert self.X is not None, "You need to call forward() first to compute the gradients!"
+        dZ_dX = 1 - self.forward(self.X) ** 2
+        gradients = self.forward(X, axis=axis)
+        return np.diagflat(gradients) - np.dot(gradients, gradients.T)
+        return dL_dZ * dZ_dX
+        """
         raise NotImplementedError
 
 
 class LogSoftmax(Softmax):
 
-    def forward(self, X: np.ndarray, axis: Optional[int] = None) -> np.ndarray:
-        return np.log(super().forward(X, axis=axis))
+    def forward(self, X: np.ndarray, axis: Optional[int] = None, **kwargs) -> np.ndarray:
+        return np.log(super().forward(X, axis=axis, **kwargs))
 
-    def gradient(self, X: np.ndarray, axis: Optional[int] = None) -> np.ndarray:
+    def backward(self, dL_dZ: np.ndarray, **kwargs) -> np.ndarray:
+        """
         gradients = self.forward(X, axis=axis)
         np.fill_diagonal(gradients, gradients.diagonal() - 1)
         return gradients
+        """
+        raise NotImplementedError

PyNeuralNet/loss.py
@@ -1,53 +1,170 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
+from typing import Optional, Union
 
 import numpy as np
 
 from PyNeuralNet.activation import Sigmoid, LogSoftmax
+from PyNeuralNet.module import Module
 
 
-class Loss(ABC):
+class Loss(Module):
+    """
+    A module representing a loss function.
+    """
 
-    def __call__(self, y_pred: np.ndarray, y_true: np.ndarray):
-        return self.forward(y_pred, y_true)
+    def __call__(
+            self,
+            y_pred: np.ndarray,
+            y_true: np.ndarray,
+            reduction: Optional[str] = "mean",
+            **kwargs
+    ) -> np.ndarray:
+        """
+        The computation of the loss between the predicted values and the targets.
+
+        Args:
+            y_pred (np.ndarray):
+                The predicted output of the model
+
+            y_true (np.ndarray):
+                The true outputs (targets)
+
+            reduction (str):
+                Controls how the loss should be aggregated across the batch.
+                If reduction := "mean": It uses np.mean(axis=0) for aggregation
+                If reduction := "sum": It uses np.sum(axis=0) for aggregation
+
+        Returns:
+            np.ndarray:
+                losses (np.ndarray):
+                    The (non-)aggregated losses between the predicted values and the targets
+        """
+        return self.forward(y_pred, y_true, reduction, **kwargs)
 
     @abstractmethod
-    def forward(self, y_pred: np.ndarray, y_true: np.ndarray):
+    def forward(
+            self,
+            y_pred: np.ndarray,
+            y_true: np.ndarray,
+            reduction: Optional[str] = "mean",
+            **kwargs
+    ) -> Union[np.ndarray, float]:
+        """
+        The computation of the loss between the predicted values and the targets.
+
+        Args:
+            y_pred (np.ndarray):
+                The predicted output of the model
+
+            y_true (np.ndarray):
+                The true outputs (targets)
+
+            reduction (str):
+                Controls how the loss should be aggregated across the batch.
+                If reduction := "mean": It uses np.mean(axis=0) for aggregation
+                If reduction := "sum": It uses np.sum(axis=0) for aggregation
+
+        Returns:
+            np.ndarray:
+                losses (np.ndarray):
+                    The (non-)aggregated losses between the predicted values and the targets
+        """
         pass
 
     @abstractmethod
-    def gradient(self, y_pred: np.ndarray, y_true: np.ndarray):
+    def backward(self, **kwargs) -> np.ndarray:
+        """
+        The Backward propagation of the loss function.
+
+        It returns the gradients dL/dy_pred of the previous passed (y_pred, y_true) from forward().
+
+        Returns:
+            np.ndarray:
+                gradients (np.ndarray):
+                    The gradients dL/dy_pred
+        """
+        pass
+
+    def update(self, params: dict[str, np.ndarray]):
         pass
 
 
 class MSE(Loss):
+    """
+    Mean Squared Error (MSE) Loss Function.
 
-    def forward(self, y_pred: np.ndarray, y_true: np.ndarray):
+    The Loss function is defined as:
+        - L(y_hat, y) = 1/N sum_i=1^N (y_hat_i - y)²
+
+    The gradients of the loss function is defined as:
+        - dL/dy_hat = 2/N * (y_hat - y)
+    """
+
+    def __init__(self):
+        self.y_pred = None
+        self.y_true = None
+
+    def forward(
+            self,
+            y_pred: np.ndarray,
+            y_true: np.ndarray,
+            reduction: Optional[str] = "mean",
+            **kwargs
+    ) -> Union[np.ndarray, float]:
         if y_true.ndim == 1:
-            # Case: y_true has shape (N,)
             y_true = np.expand_dims(y_true, axis=-1)
 
         if y_pred.shape != y_true.shape:
             raise ValueError(
-                f"Shape mismatch. The shape of y_pred ({y_pred.shape}) and y_true ({y_true.shape}) should be equal!"
+                f"Illegal y_pred {y_pred.shape} and y_true {y_true.shape}. The shapes should be equal!"
             )
-        return np.mean((y_pred - y_true) ** 2, axis=-1, keepdims=True)
 
-    def gradient(self, y_pred: np.ndarray, y_true: np.ndarray):
-        if y_true.ndim == 1:
-            y_true = np.expand_dims(y_true, axis=-1)
+        reduction_wrapper = {
+            "mean": np.mean,
+            "sum": np.sum,
+            "none": lambda x: x,
+        }
+        if reduction not in reduction_wrapper.keys():
+            raise ValueError(f"Illegal reduction {reduction}! The valid options are {reduction_wrapper.keys()}!")
 
-        if y_pred.shape != y_true.shape:
-            raise ValueError(
-                f"Shape mismatch. The shape of y_pred ({y_pred.shape}) and y_true ({y_true.shape}) should be equal!"
-            )
-        return (2 / y_pred.shape[-1]) * (y_pred - y_true)
+        # Safe the inputs for the computation of gradients
+        self.y_pred = y_pred
+        self.y_true = y_true
+
+        # Compute the losses for every sample
+        losses = np.mean((y_pred - y_true) ** 2, axis=-1, keepdims=True)
+
+        return reduction_wrapper[reduction](losses)
+
+    def backward(self, **kwargs) -> np.ndarray:
+        if self.y_pred is None or self.y_true is None:
+            raise ValueError("You need to call forward() first before to compute the gradients!")
+        return (2 / self.y_pred.shape[-1]) * (self.y_pred - self.y_true)
 
 
 class MAE(Loss):
+    """
+    Mean Absolute Error (MAE) Loss Function.
 
-    def forward(self, y_pred: np.ndarray, y_true: np.ndarray):
+    The Loss function is defined as:
+        - L(y_hat, y) = 1/N sum_i=1^N |y_hat_i - y|
+
+    The gradients of the loss function is defined as:
+        - dL/dy_hat = 1/N * sign(y_hat - y)
+    """
+
+    def __init__(self):
+        self.y_pred = None
+        self.y_true = None
+
+    def forward(
+            self,
+            y_pred: np.ndarray,
+            y_true: np.ndarray,
+            reduction: Optional[str] = "mean",
+            **kwargs
+    ) -> Union[np.ndarray, float]:
         if y_true.ndim == 1:
-            # Case: y_true has shape (N,)
             y_true = np.expand_dims(y_true, axis=-1)
 
         if y_pred.shape != y_true.shape:
@@ -55,24 +172,51 @@ class MAE(Loss):
                 f"Shape mismatch. The shape of y_pred ({y_pred.shape}) and y_true ({y_true.shape}) should be equal!"
             )
 
-        return np.mean(np.abs(y_pred - y_true), axis=-1, keepdims=True)
+        reduction_wrapper = {
+            "mean": np.mean,
+            "sum": np.sum,
+            "none": lambda x: x,
+        }
+        if reduction not in reduction_wrapper.keys():
+            raise ValueError(f"Illegal reduction {reduction}! The valid options are {reduction_wrapper.keys()}!")
 
-    def gradient(self, y_pred: np.ndarray, y_true: np.ndarray):
-        if y_true.ndim == 1:
-            # Case: y_true has shape (N,)
-            y_true = np.expand_dims(y_true, axis=-1)
+        # Safe the inputs for the computation of gradients
+        self.y_pred = y_pred
+        self.y_true = y_true
 
-        if y_pred.shape != y_true.shape:
-            raise ValueError(
-                f"Shape mismatch. The shape of y_pred ({y_pred.shape}) and y_true ({y_true.shape}) should be equal!"
-            )
+        # Compute the losses for every sample
+        losses = np.mean(np.abs(y_pred - y_true), axis=-1, keepdims=True)
 
-        return (1 / y_pred.shape[-1]) * np.sign(y_pred - y_true)
+        return reduction_wrapper[reduction](losses)
+
+    def backward(self, **kwargs) -> np.ndarray:
+        if self.y_pred is None or self.y_true is None:
+            raise ValueError("You need to call forward() first before to compute the gradients!")
+        return (1 / self.y_pred.shape[-1]) * np.sign(self.y_pred - self.y_true)
 
 
 class BCEWithLogits(Loss):
+    """
+    Binary Cross Entropy (with Logits) (BCE) Loss Function.
 
-    def forward(self, y_pred: np.ndarray, y_true: np.ndarray):
+    The Loss function is defined as:
+        - L(y_logits, y) = - 1/N sum_i=1^N y_i * log(sigmoid(y_logits_i)) + (1 - y_i) * log(sigmoid(1 - y_logits_i))
+
+    The gradients of the loss function is defined as:
+        - dL/dy_logits = 1/N * sigmoid(y_logits) - y
+    """
+
+    def __init__(self):
+        self.y_pred = None
+        self.y_true = None
+
+    def forward(
+            self,
+            y_pred: np.ndarray,
+            y_true: np.ndarray,
+            reduction: Optional[str] = "mean",
+            **kwargs
+    ) -> Union[np.ndarray, float]:
         # Change the shape (N,) to (N, 1)
         y_true = np.expand_dims(y_true, axis=-1)
 
@@ -81,56 +225,83 @@ class BCEWithLogits(Loss):
                 f"Shape mismatch. The shape of y_pred ({y_pred.shape}) and y_true ({y_true.shape}) should be equal!"
             )
 
+        reduction_wrapper = {
+            "mean": np.mean,
+            "sum": np.sum,
+            "none": lambda x: x,
+        }
+        if reduction not in reduction_wrapper.keys():
+            raise ValueError(f"Illegal reduction {reduction}! The valid options are {reduction_wrapper.keys()}!")
+
         # Apply a Sigmoid on y_pred (from logits to probabilities)
         y_pred = Sigmoid()(y_pred)
+
+        self.y_pred = y_pred
+        self.y_true = y_true
 
         zero_term = (1 - y_true) * np.log(1 - y_pred)
         one_term = y_true * np.log(y_pred)
-        return -np.mean(zero_term + one_term, axis=-1, keepdims=True)
 
-    def gradient(self, y_pred: np.ndarray, y_true: np.ndarray):
-        if y_true.ndim == 1:
-            # Case: y_true has shape (N,)
-            y_true = np.expand_dims(y_true, axis=-1)
+        # Compute the losses for every sample
+        losses = -np.mean(zero_term + one_term, axis=-1, keepdims=True)
 
-        if y_pred.shape != y_true.shape:
-            raise ValueError(
-                f"Shape mismatch. The shape of y_pred ({y_pred.shape}) and y_true ({y_true.shape}) should be equal!"
-            )
+        return reduction_wrapper[reduction](losses)
 
-        # Apply a Sigmoid on y_pred (from logits to probabilities)
-        y_pred = Sigmoid()(y_pred)
-
-        return y_pred - y_true
+    def backward(self, **kwargs) -> np.ndarray:
+        if self.y_pred is None or self.y_true is None:
+            raise ValueError("You need to call forward() first before to compute the gradients!")
+        return self.y_pred - self.y_true
 
 
 class CEWithLogits(Loss):
+    # TODO: Fix Errors
+    def __init__(self):
+        self.y_pred = None
+        self.y_true = None
 
-    def forward(self, y_pred: np.ndarray, y_true: np.ndarray):
+    def forward(
+            self,
+            y_pred: np.ndarray,
+            y_true: np.ndarray,
+            reduction: Optional[str] = "mean",
+            **kwargs
+    ) -> Union[np.ndarray, float]:
         if y_pred.shape[0] != y_true.shape[0]:
             raise ValueError(
                 f"Shape mismatch. First axis of y_pred ({y_pred.shape}) and y_true ({y_true.shape}) should be equal!"
             )
 
+        reduction_wrapper = {
+            "mean": np.mean,
+            "sum": np.sum,
+            "none": lambda x: x,
+        }
+        if reduction not in reduction_wrapper.keys():
+            raise ValueError(f"Illegal reduction {reduction}! The valid options are {reduction_wrapper.keys()}!")
+
         # Apply a log-softmax on y_pred (from logits to probabilities)
         y_pred = LogSoftmax()(y_pred, axis=-1)
+
+        self.y_pred = y_pred
+        self.y_true = y_true
 
         # Get the corresponding probabilities of the true classes (columns)
         rows = np.arange(len(y_pred))
         y_pred = np.expand_dims(y_pred[rows, y_true], axis=-1)
 
-        return np.mean(-y_pred, axis=-1, keepdims=True)
+        # Compute the losses for every sample
+        losses = np.mean(-y_pred, axis=-1, keepdims=True)
 
-    def gradient(self, y_pred: np.ndarray, y_true: np.ndarray):
-        if y_pred.shape[0] != y_true.shape[0]:
-            raise ValueError(
-                f"Shape mismatch. First axis of y_pred ({y_pred.shape}) and y_true ({y_true.shape}) should be equal!"
-            )
+        return reduction_wrapper[reduction](losses)
+
+    def backward(self, **kwargs) -> np.ndarray:
+        if self.y_pred is None or self.y_true is None:
+            raise ValueError("You need to call forward() first before to compute the gradients!")
 
         # Apply a log-softmax on y_pred (from logits to probabilities)
-        gradients = LogSoftmax()(y_pred, axis=-1)
+        gradients = LogSoftmax()(self.y_pred, axis=-1)
 
         # Get the corresponding probabilities for each true class
-        rows = np.arange(len(y_pred))
-        gradients[rows, y_true] -= 1
+        rows = np.arange(len(self.y_pred))
+        gradients[rows, self.y_true] -= 1
         return gradients

PyNeuralNet/module.py
@@ -5,16 +5,118 @@ import numpy as np
 
 class Module(ABC):
 
-    def __call__(self, X: np.ndarray, **kwargs) -> np.ndarray:
-        return self.forward(X, **kwargs)
+    def __call__(self, *args, **kwargs) -> np.ndarray:
+        """
+        The forward pass of the module.
+
+        Args:
+            *args:
+                The input of the module
+
+        Returns:
+            np.ndarray:
+                The output of the module (Z)
+        """
+        return self.forward(*args, **kwargs)
 
     @abstractmethod
-    def forward(self, X: np.ndarray, **kwargs) -> np.ndarray:
+    def forward(self, *args, **kwargs) -> np.ndarray:
+        """
+        The forward pass of the module.
+
+        Args:
+            *args:
+                The input of the module
+
+        Returns:
+            np.ndarray:
+                The output of the module (Z)
+        """
         # The forward pass of the model
         pass
 
     @abstractmethod
-    def gradient(self, X: np.ndarray, **kwargs) -> np.ndarray:
-        # Computes the gradient dZ/dX ...
+    def backward(self, *args, **kwargs) -> np.ndarray:
+        """
+        The Backward propagation of a module.
+
+        A module is defined as a function f: X -> Z, which
+        transforms an input X into the output Z.
+
+        The Backward propagation of a module is defined by
+        computing the gradients dL/dX with the chain rule:
+
+        - dL/dX = dL/dZ * dZ/dX
+
+        Args:
+            *args:
+                The gradients dL/dZ of the output Z
+
+        Returns:
+            np.ndarray:
+                The gradients dL/dX of the input X
+        """
         pass
 
+    @abstractmethod
+    def update(self, params: dict[str, dict[str, np.ndarray]]):
+        """
+        Updates the parameters of a module by the given values.
+
+        Args:
+            params (dict[str, dict[str, np.ndarray]]):
+                module_name (str):
+                    The name of the module
+
+                module (dict[str, np.ndarray]):
+
+                    key (str):
+                        The name of the tunable parameter
+
+                    value (np.ndarray):
+                        The value of the tunable parameter
+        """
+        pass
+
+    def parameters(self, **kwargs) -> dict[str, dict[str, np.ndarray]]:
+        """
+        Returns the parameters of the module, which
+        should be optimized with gradient descent.
+
+        Returns:
+            dict[str, dict[str, np.ndarray]]:
+
+                module_name (str):
+                    The name of the module
+
+                module (dict[str, np.ndarray]):
+
+                    key (str):
+                        The name of the tunable parameter
+
+                    value (np.ndarray):
+                        The value of the tunable parameter
+        """
+        return {"module_0": {}}
+
+    def gradients(self, **kwargs) -> dict[str, dict[str, np.ndarray]]:
+        """
+        Returns the gradients of each parameter of the
+        module, which should be optimized with gradient
+        descent.
+
+        Returns:
+            dict[str, dict[str, np.ndarray]]:
+
+                module_name (str):
+                    The name of the module
+
+                module (dict[str, np.ndarray]):
+
+                    key (str):
+                        The name of the tunable parameter
+
+                    value (np.ndarray):
+                        The gradients dL/dtheta of the tunable parameter
+        """
+        return {"module_0": {}}

tests/test_activation.py
@@ -1,4 +1,5 @@
 import unittest
+
 import numpy as np
 
 from PyNeuralNet.activation import ReLU, LeakyReLU, Sigmoid, Tanh, Softmax, LogSoftmax
@@ -14,6 +15,11 @@ class TestReLU(unittest.TestCase):
             [0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
+        ])
+        self.dL_dZ = np.array([
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
         ])
         self.act_fn = ReLU()
 
@@ -39,16 +45,29 @@ class TestReLU(unittest.TestCase):
             [6, 7, 8],
         ]), Z)
 
-    def test_gradient(self):
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the function backward().
         """
-        dZ_dX = self.act_fn.gradient(self.X)
+        self.act_fn.forward(self.X)
+        dL_dX = self.act_fn.backward(self.dL_dZ)
         np.testing.assert_almost_equal(np.array([
             [0, 1, 1],
             [1, 1, 1],
             [1, 1, 1],
-        ]), dZ_dX)
+        ]), dL_dX)
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.gradients())
 
 
 class TestLeakyReLU(unittest.TestCase):
@@ -61,6 +80,11 @@ class TestLeakyReLU(unittest.TestCase):
             [0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
+        ])
+        self.dL_dZ = np.array([
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
         ])
         self.act_fn = LeakyReLU(slope=0.1)
 
@@ -86,27 +110,46 @@ class TestLeakyReLU(unittest.TestCase):
             [6, 7, 8],
         ]), Z)
 
-    def test_gradient(self):
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
-        dZ_dX = self.act_fn.gradient(self.X)
+        self.act_fn.forward(self.X)
+        dL_dX = self.act_fn.backward(self.dL_dZ)
         np.testing.assert_almost_equal(np.array([
             [0.1, 1, 1],
             [1, 1, 1],
             [1, 1, 1],
-        ]), dZ_dX)
+        ]), dL_dX)
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.gradients())
 
 
 class TestSigmoid(unittest.TestCase):
     """
     Tests the class Sigmoid.
     """
+
     def setUp(self):
         self.X = np.array([
             [0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
+        ])
+        self.dL_dZ = np.array([
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
         ])
         self.act_fn = Sigmoid()
 
@@ -132,27 +175,46 @@ class TestSigmoid(unittest.TestCase):
             [0.99752738, 0.99908895, 0.99966465],
         ]), Z)
 
-    def test_gradient(self):
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
-        dZ_dX = self.act_fn.gradient(self.X)
+        self.act_fn.forward(self.X)
+        dL_dX = self.act_fn.backward(self.dL_dZ)
         np.testing.assert_almost_equal(np.array([
             [0.25, 0.19661193, 0.10499359],
             [0.04517666, 0.01766271, 0.00664806],
             [0.00246651, 0.00091022, 0.00033524],
-        ]), dZ_dX)
+        ]), dL_dX)
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.gradients())
 
 
 class TestTanh(unittest.TestCase):
     """
     Tests the class Tanh.
     """
+
     def setUp(self):
         self.X = np.array([
             [0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
+        ])
+        self.dL_dZ = np.array([
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
         ])
         self.act_fn = Tanh()
 
@@ -178,16 +240,29 @@ class TestTanh(unittest.TestCase):
             [0.99998771, 0.99999834, 0.99999977],
         ]), Z)
 
-    def test_gradient(self):
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
-        dZ_dX = self.act_fn.gradient(self.X)
+        self.act_fn.forward(self.X)
+        dL_dX = self.act_fn.backward(self.dL_dZ)
         np.testing.assert_almost_equal(np.array([
             [1.00000000e+00, 4.19974342e-01, 7.06508249e-02],
             [9.86603717e-03, 1.34095068e-03, 1.81583231e-04],
             [2.45765474e-05, 3.32610934e-06, 4.50140597e-07],
-        ]), dZ_dX)
+        ]), dL_dX)
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.gradients())
 
 
 class TestSoftmax(unittest.TestCase):
@@ -255,12 +330,33 @@ class TestSoftmax(unittest.TestCase):
         ]), Z)
         np.testing.assert_almost_equal(np.array([1.0, 1.0, 1.0]), np.sum(Z, axis=1))
 
-    def test_gradient(self):
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
         # TODO: Implement here
+        """
+        dZ_dX = self.act_fn.gradients(self.X)
+        print(dZ_dX.shape)
+        np.testing.assert_almost_equal(np.array([
+            [-9.45855173, -7.45855173, -6.45855173],
+            [-5.45855173, -5.45855173, -3.45855173],
+            [-2.45855173, -1.45855173, -1.45855173],
+        ]), dZ_dX)
+        """
         pass
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.gradients())
 
 
 class TestLogSoftmax(unittest.TestCase):
@@ -326,16 +422,32 @@ class TestLogSoftmax(unittest.TestCase):
             [-2.40760596, -1.40760596, -0.40760596],
         ]), Z)
 
-    def test_gradient(self):
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
-        dZ_dX = self.act_fn.gradient(self.X)
+        # TODO: Implement here
+        """
+        dZ_dX = self.act_fn.backward(self.X)
         np.testing.assert_almost_equal(np.array([
             [-9.45855173, -7.45855173, -6.45855173],
             [-5.45855173, -5.45855173, -3.45855173],
             [-2.45855173, -1.45855173, -1.45855173],
         ]), dZ_dX)
+        """
+        pass
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.act_fn.gradients())
 
 
 if __name__ == '__main__':

tests/test_loss.py
@@ -28,7 +28,7 @@ class TestMSE(unittest.TestCase):
         """
         Tests the magic method __call__().
         """
-        loss = self.loss_fn(self.y_pred, self.y_true)
+        loss1 = self.loss_fn(self.y_pred, self.y_true, reduction="none")
         np.testing.assert_almost_equal(np.array([
             [1],
             [0],
@@ -39,13 +39,19 @@ class TestMSE(unittest.TestCase):
             [25],
             [36],
             [49],
-        ]), loss)
+        ]), loss1)
+
+        loss2 = self.loss_fn(self.y_pred, self.y_true, reduction="mean")
+        np.testing.assert_almost_equal(np.array([15.66666667]), loss2)
+
+        loss3 = self.loss_fn(self.y_pred, self.y_true, reduction="sum")
+        np.testing.assert_almost_equal(np.array([141.0]), loss3)
 
     def test_forward(self):
         """
         Tests the method forward().
         """
-        loss = self.loss_fn.forward(self.y_pred, self.y_true)
+        loss1 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="none")
         np.testing.assert_almost_equal(np.array([
             [1],
             [0],
@@ -56,13 +62,20 @@ class TestMSE(unittest.TestCase):
             [25],
             [36],
             [49],
-        ]), loss)
+        ]), loss1)
 
-    def test_gradient(self):
+        loss2 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="mean")
+        np.testing.assert_almost_equal(np.array([15.66666667]), loss2)
+
+        loss3 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="sum")
+        np.testing.assert_almost_equal(np.array([141.0]), loss3)
+
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
-        dL_dy_pred = self.loss_fn.gradient(self.y_pred, self.y_true)
+        self.loss_fn.forward(self.y_pred, self.y_true)
+        dL_dy_pred = self.loss_fn.backward()
         np.testing.assert_almost_equal(np.array([
             [-2],
             [0],
@@ -75,10 +88,22 @@ class TestMSE(unittest.TestCase):
             [14],
         ]), dL_dy_pred)
 
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.loss_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.loss_fn.gradients())
+
 
 class TestMAE(unittest.TestCase):
     """
-    Tests the class MAE
+    Tests the class MAE.
     """
 
     def setUp(self):
@@ -100,7 +125,7 @@ class TestMAE(unittest.TestCase):
         """
         Tests the magic method __call__().
         """
-        loss = self.loss_fn(self.y_pred, self.y_true)
+        loss1 = self.loss_fn(self.y_pred, self.y_true, reduction="none")
         np.testing.assert_almost_equal(np.array([
             [1],
             [0],
@@ -111,13 +136,19 @@ class TestMAE(unittest.TestCase):
             [5],
             [6],
             [7],
-        ]), loss)
+        ]), loss1)
+
+        loss2 = self.loss_fn(self.y_pred, self.y_true, reduction="mean")
+        np.testing.assert_almost_equal(np.array([3.22222222]), loss2)
+
+        loss3 = self.loss_fn(self.y_pred, self.y_true, reduction="sum")
+        np.testing.assert_almost_equal(np.array([29.0]), loss3)
 
     def test_forward(self):
         """
         Tests the method forward().
         """
-        loss = self.loss_fn.forward(self.y_pred, self.y_true)
+        loss1 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="none")
         np.testing.assert_almost_equal(np.array([
             [1],
             [0],
@@ -128,13 +159,20 @@ class TestMAE(unittest.TestCase):
             [5],
             [6],
             [7],
-        ]), loss)
+        ]), loss1)
 
-    def test_gradient(self):
+        loss2 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="mean")
+        np.testing.assert_almost_equal(np.array([3.22222222]), loss2)
+
+        loss3 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="sum")
+        np.testing.assert_almost_equal(np.array([29.0]), loss3)
+
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
-        dL_dy_pred = self.loss_fn.gradient(self.y_pred, self.y_true)
+        self.loss_fn.forward(self.y_pred, self.y_true)
+        dL_dy_pred = self.loss_fn.backward()
         np.testing.assert_almost_equal(np.array([
             [-1],
             [0],
@@ -146,6 +184,18 @@ class TestMAE(unittest.TestCase):
             [1],
             [1],
         ]), dL_dy_pred)
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.loss_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.loss_fn.gradients())
 
 
 class TestBCEWithLogits(unittest.TestCase):
@@ -172,7 +222,7 @@ class TestBCEWithLogits(unittest.TestCase):
         """
         Tests the magic method __call__().
         """
-        loss = self.loss_fn(self.y_pred, self.y_true)
+        loss1 = self.loss_fn(self.y_pred, self.y_true, reduction="none")
         np.testing.assert_almost_equal(np.array([
             [6.93147181e-01],
             [3.13261688e-01],
@@ -183,13 +233,19 @@ class TestBCEWithLogits(unittest.TestCase):
             [2.47568514e-03],
             [9.11466454e-04],
             [3.35406373e-04],
-        ]), loss)
+        ]), loss1)
+
+        loss2 = self.loss_fn(self.y_pred, self.y_true, reduction="mean")
+        np.testing.assert_almost_equal(np.array([0.13450134]), loss2)
+
+        loss3 = self.loss_fn(self.y_pred, self.y_true, reduction="sum")
+        np.testing.assert_almost_equal(np.array([1.21051207]), loss3)
 
     def test_forward(self):
         """
         Tests the method forward().
         """
-        loss = self.loss_fn.forward(self.y_pred, self.y_true)
+        loss1 = (self.loss_fn.forward(self.y_pred, self.y_true, reduction="none"))
         np.testing.assert_almost_equal(np.array([
             [6.93147181e-01],
             [3.13261688e-01],
@@ -200,13 +256,20 @@ class TestBCEWithLogits(unittest.TestCase):
             [2.47568514e-03],
             [9.11466454e-04],
             [3.35406373e-04],
-        ]), loss)
+        ]), loss1)
 
-    def test_gradient(self):
+        loss2 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="mean")
+        np.testing.assert_almost_equal(np.array([0.13450134]), loss2)
+
+        loss3 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="sum")
+        np.testing.assert_almost_equal(np.array([1.21051207]), loss3)
+
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
-        dL_dz = self.loss_fn.gradient(self.y_pred, self.y_true)
+        self.loss_fn.forward(self.y_pred, self.y_true)
+        dL_dz = self.loss_fn.backward()
         np.testing.assert_almost_equal(np.array([
             [-5.00000000e-01],
             [-2.68941421e-01],
@@ -218,6 +281,18 @@ class TestBCEWithLogits(unittest.TestCase):
             [-9.11051194e-04],
             [-3.35350130e-04],
         ]), dL_dz)
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.loss_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.loss_fn.gradients())
 
 
 class TestCEWithLogits(unittest.TestCase):
@@ -241,7 +316,7 @@ class TestCEWithLogits(unittest.TestCase):
         """
         Tests the magic method __call__().
         """
-        loss = self.loss_fn(self.y_pred, self.y_true)
+        loss1 = self.loss_fn(self.y_pred, self.y_true, reduction="none")
         np.testing.assert_almost_equal(np.array([
             [1.40760596],
             [1.40760596],
@@ -252,13 +327,19 @@ class TestCEWithLogits(unittest.TestCase):
             [1.40760596],
             [1.40760596],
             [1.40760596],
-        ]), loss)
+        ]), loss1)
+
+        loss2 = self.loss_fn(self.y_pred, self.y_true, reduction="mean")
+        np.testing.assert_almost_equal(np.array([1.40760596]), loss2)
+
+        loss3 = self.loss_fn(self.y_pred, self.y_true, reduction="sum")
+        np.testing.assert_almost_equal(np.array([12.66845368]), loss3)
 
     def test_forward(self):
         """
         Tests the method forward().
         """
-        loss = self.loss_fn.forward(self.y_pred, self.y_true)
+        loss1 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="none")
         np.testing.assert_almost_equal(np.array([
             [1.40760596],
             [1.40760596],
@@ -269,13 +350,20 @@ class TestCEWithLogits(unittest.TestCase):
             [1.40760596],
             [1.40760596],
             [1.40760596],
-        ]), loss)
+        ]), loss1)
 
-    def test_gradient(self):
+        loss2 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="mean")
+        np.testing.assert_almost_equal(np.array([1.40760596]), loss2)
+
+        loss3 = self.loss_fn.forward(self.y_pred, self.y_true, reduction="sum")
+        np.testing.assert_almost_equal(np.array([12.66845368]), loss3)
+
+    def test_backward(self):
         """
-        Tests the method gradient().
+        Tests the method backward().
         """
-        dL_dz = self.loss_fn.gradient(self.y_pred, self.y_true)
+        self.loss_fn.forward(self.y_pred, self.y_true)
+        dL_dz = self.loss_fn.backward()
         np.testing.assert_almost_equal(np.array([
             [-2.40760596, -2.40760596, -0.40760596],
             [-2.40760596, -2.40760596, -0.40760596],
@@ -287,6 +375,18 @@ class TestCEWithLogits(unittest.TestCase):
             [-2.40760596, -2.40760596, -0.40760596],
             [-2.40760596, -2.40760596, -0.40760596],
         ]), dL_dz)
+
+    def test_parameters(self):
+        """
+        Tests the method parameters().
+        """
+        self.assertEqual({"module_0": {}}, self.loss_fn.parameters())
+
+    def test_gradients(self):
+        """
+        Tests the method gradients().
+        """
+        self.assertEqual({"module_0": {}}, self.loss_fn.parameters())
 
 
 if __name__ == '__main__':